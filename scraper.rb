155f9af453696f347c7ef213678d7471829f2181
Luke Bacon <luke@equivalentideas.com>
2016-05-20 15:48:36 +1000

    Parse the data out of the CSV

--- scraper.rb
+++ scraper.rb
@@ -2,13 +2,17 @@
 # including some code snippets below that you should find helpful
 
 require 'scraperwiki'
-require 'mechanize'
 require 'pry'
+require 'csv'
 
 # This is the CSV export URL for the spreadsheet provided by the fine folks at  dlgc.wa.gov.au:
 # https://docs.google.com/spreadsheets/d/1J1SShMPYBuGVAHY7LfDiTiHV72Tph8ObxbhlCoo7nn8/edit#gid=1431929499
 def google_sheets_export_url
   "https://docs.google.com/spreadsheets/d/1J1SShMPYBuGVAHY7LfDiTiHV72Tph8ObxbhlCoo7nn8/export?format=csv&id=1J1SShMPYBuGVAHY7LfDiTiHV72Tph8ObxbhlCoo7nn8&gid=1431929499"
+end
+
+def parse_name(text)
+  text.sub(/\((M|F)\)$/, "").strip.split(", ").reverse.join(" ").strip
 end
 
 # Remove councillor whatnot
@@ -27,46 +31,27 @@ def create_id(council, name)
   components.downcase.gsub(" ","_")
 end
 
-def scrape_council_page(page)
-  council = page.at(:h1).text + " Council"
-  puts "Scraping councillors for #{council}"
+CSV.parse(open(google_sheets_export_url).read)[1..-1].each do |row|
+  name = parse_name(row[2])
 
-  council_data_keys = page.at("h1 + div").search(:strong)
-  council_email = council_data_keys.select {|key| key.text.include? "Email:" }[0].next_sibling.text
-  council_website = council_data_keys.select {|key| key.text.include? "site:" }[0].next_element[:href]
+  council = row.first + " Council"
 
-  mayor_name = simplify_name(council_data_keys.select {|key| key.text.include? "Mayor:" }[0].next_sibling.text)
-  deputy_mayor_name = simplify_name(council_data_keys.select {|key| key.text.include? "Deputy" }[0].next_sibling.text)
-
-  councillor_table = page.at(:table)
-  councillors = councillor_table.search(:tr)[1..-1]
-  councillors.each do |councillor_element|
-    name = simplify_name(councillor_element.at(:td).text)
-
-    executive = ""
-    if name.eql? mayor_name
-      executive = "Mayor"
-    elsif name.eql? deputy_mayor_name
-      executive = "Deputy Mayor"
-    end
-
-    p councillor = {
-      name: name,
-      executive: executive,
-      council: council,
-      # council_website: council_website,
-      id: create_id(council, name),
-      # council_email: council_email,
-      # term_ends: councillor_element.search(:td).last.text
-      gender: ""
-    }
-
-    ScraperWiki.save_sqlite([:id], councillor)
+  gender = if row[2].end_with? "(F)"
+    "female"
+  elsif row[2].end_with? "(M)"
+    "male"
+  else
+    nil
   end
-end
 
-agent = Mechanize.new
-index_page = agent.get("http://www.dpac.tas.gov.au/divisions/local_government/local_government_directory/councils")
-index_page.at(:table).search(:a).each do |a|
-  scrape_council_page(agent.get(a[:href]))
+  p councillor = {
+    name: name,
+    executive: row[1] =~ /(mayor){1}/i ? "Mayor" : nil,
+    council: council,
+    ward: row[1],
+    id: create_id(council, name),
+    gender: gender
+  }
+
+  ScraperWiki.save_sqlite([:id], councillor)
 end
